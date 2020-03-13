9de6ac0892738015e27271730e4f45c3b72f3d6f
Bohan <gk6511@wayne.edu>
2020-03-13 15:07:08 -0400

    add pageHelper

--- src/main/java/com/bohan/controller/TestController.java
+++ src/main/java/com/bohan/controller/TestController.java
@@ -4,9 +4,12 @@ package com.bohan.controller;
 import com.bohan.config.WechatConfig;
 import com.bohan.entity.Video;
 import com.bohan.service.impl.VideoServiceImpl;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -34,5 +37,15 @@ public class TestController {
     @GetMapping("/testVideo")
     public List<Video> testVideo(){
         return videoService.findAll();
+    }
+
+    @GetMapping("/testPageHelper")
+    public PageInfo<Video> testPageHelper(@RequestParam(value = "page", defaultValue = "1")int page,
+                                          @RequestParam(value = "size", defaultValue = "10")int size){
+        PageHelper.startPage(page, size);
+        List<Video> videos = videoService.findAll();
+        PageInfo<Video> pageInfo = new PageInfo<>(videos);
+        return pageInfo;
+
     }
 }
